46c2bc4c7eb5e0b52ad539869b8dc9862c5b8715
Patrick Foh <patrickfoh@gmail.com>
2018-04-01 23:03:52 +0100

    Add InvalidResponseException

---
+++ src/Exceptions/InvalidResponseException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 01/04/2018
+ * Time: 22:52
+ */
+
+namespace Mrfoh\Mulla\Exceptions;
+
+use Exception;
+
+class InvalidResponseException extends Exception {}
